acf3d1c2b60855c058268af99dc5a9c2b3d0b4b9
Andrew <andrew.1.kulikov@gmail.com>
2019-03-25 23:50:12 +0300

    Remove all rerender and data storing from worklet

--- src/js/board.js
+++ src/js/board.js
@@ -1,85 +1,11 @@
-let cells = [];
-const cellsCount = 64;
-let name = 0;
-
 class GameOfLifePainter {
-  constructor() {
-    this.name = Math.random();
-    console.log(name);
-
-    if (!name) name = this.name;
-
-    console.log("name", name);
-    this.canvasSize = 512;
-
-    this.init();
+  static get inputProperties() {
+    return ["--cells"];
   }
 
-  init() {
-    console.log("init");
-    for (let i = 0; i < cellsCount; i++) {
-      cells[i] = [];
-      for (let j = 0; j < cellsCount; j++) {
-        cells[i][j] = 0;
-      }
-    }
+  
 
-    // Gosper glider gun
-    const coordinates = [
-      [1, 5],
-      [1, 6],
-      [2, 5],
-      [2, 6],
-      [11, 5],
-      [11, 6],
-      [11, 7],
-      [12, 4],
-      [12, 8],
-      [13, 3],
-      [13, 9],
-      [14, 3],
-      [14, 9],
-      [15, 6],
-      [16, 4],
-      [16, 8],
-      [17, 5],
-      [17, 6],
-      [17, 7],
-      [18, 6],
-      [21, 3],
-      [21, 4],
-      [21, 5],
-      [22, 3],
-      [22, 4],
-      [22, 5],
-      [23, 2],
-      [23, 6],
-      [25, 1],
-      [25, 2],
-      [25, 6],
-      [25, 7],
-      [35, 3],
-      [35, 4],
-      [36, 3],
-      [36, 4],
-
-      [60, 47],
-      [61, 47],
-      [62, 47],
-      [60, 48],
-      [61, 48],
-      [62, 48],
-      [60, 49],
-      [61, 49],
-      [62, 49],
-      [60, 51],
-      [61, 51],
-      [62, 51]
-    ];
-    coordinates.forEach(point => (cells[point[0]][point[1]] = 1));
-  }
-
-  update() {
+  update(cells) {
     let result = [];
 
     function _countNeighbours(x, y) {
@@ -118,10 +44,20 @@ class GameOfLifePainter {
   }
 
   paint(ctx, geom, properties) {
-    console.log("asdasdasdasd", this.name, name);
-    if (name !== this.name) return;
-    this.update();
-    const cellSize = geom.height / cellsCount;
+    const positions = properties
+      .get("--cells")[0]
+      .trim()
+      .split(" ")
+      .map(v => v.split(",").map(x => parseInt(x)));
+
+    const cells = positions;
+
+    //TODO: move to props
+    const cellsCount = 64;
+    //return;
+    //this.update();
+    const cellHeight = geom.height / cellsCount;
+    const cellWidth = geom.width / cellsCount;
     ctx.strokeStyle = "#e1e1e1";
     ctx.fillStyle = "cadetblue";
 
@@ -129,7 +65,7 @@ class GameOfLifePainter {
     cells.forEach((row, x) => {
       row.forEach((cell, y) => {
         ctx.beginPath();
-        ctx.rect(x * cellSize, y * cellSize, cellSize, cellSize);
+        ctx.rect(x * cellWidth, y * cellHeight, cellWidth, cellHeight);
         if (cell) {
           ctx.fill();
         } else {
