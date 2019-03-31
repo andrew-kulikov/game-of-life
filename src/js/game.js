79626f94611760a10beead155e66d6de9e925a7b
Andrew <andrew.1.kulikov@gmail.com>
2019-03-31 22:59:17 +0300

    Move js from html and add stop button

--- src/js/game.js
+++ src/js/game.js
@@ -1,147 +1,127 @@
-let canvas = document.getElementById("myCanvas").getContext("2d");
+let game = document.getElementById("game");
+CSS.paintWorklet.addModule("js/board.js");
 
-canvas.strokeStyle = "#e1e1e1";
-canvas.fillStyle = "cadetblue";
+let frame = 1;
 
-class GameOfLife {
-  constructor(cellsCount, canvasSize, canvas) {
-    this.cellsCount = cellsCount;
-    this.canvasSize = canvasSize;
-    this.canvas = canvas;
-    this.cells = [];
+const coordinates = [
+  [1, 5],
+  [1, 6],
+  [2, 5],
+  [2, 6],
+  [11, 5],
+  [11, 6],
+  [11, 7],
+  [12, 4],
+  [12, 8],
+  [13, 3],
+  [13, 9],
+  [14, 3],
+  [14, 9],
+  [15, 6],
+  [16, 4],
+  [16, 8],
+  [17, 5],
+  [17, 6],
+  [17, 7],
+  [18, 6],
+  [21, 3],
+  [21, 4],
+  [21, 5],
+  [22, 3],
+  [22, 4],
+  [22, 5],
+  [23, 2],
+  [23, 6],
+  [25, 1],
+  [25, 2],
+  [25, 6],
+  [25, 7],
+  [35, 3],
+  [35, 4],
+  [36, 3],
+  [36, 4],
 
-    this.init();
+  [60, 47],
+  [61, 47],
+  [62, 47],
+  [60, 48],
+  [61, 48],
+  [62, 48],
+  [60, 49],
+  [61, 49],
+  [62, 49],
+  [60, 51],
+  [61, 51],
+  [62, 51]
+];
+
+const update = () => {
+  function _countNeighbours(x, y) {
+    let amount = 0;
+
+    const _isAlive = (x, y) => cells[x] && cells[x][y];
+    const offsets = [-1, 0, 1];
+
+    offsets.forEach(offsetX =>
+      offsets.forEach(offsetY => (amount += _isAlive(x + offsetX, y + offsetY)))
+    );
+    amount -= _isAlive(x, y);
+
+    return amount;
   }
 
-  init() {
-    for (let i = 0; i < this.cellsCount; i++) {
-      this.cells[i] = [];
-      for (let j = 0; j < this.cellsCount; j++) {
-        this.cells[i][j] = 0;
-      }
-    }
+  let result = [];
+  cellsPositions = [];
+  cells.forEach((row, x) => {
+    result[x] = [];
+    row.forEach((cell, y) => {
+      let alive = 0,
+        count = _countNeighbours(x, y);
 
-    [
-      // Gosper glider gun
-      [1, 5],
-      [1, 6],
-      [2, 5],
-      [2, 6],
-      [11, 5],
-      [11, 6],
-      [11, 7],
-      [12, 4],
-      [12, 8],
-      [13, 3],
-      [13, 9],
-      [14, 3],
-      [14, 9],
-      [15, 6],
-      [16, 4],
-      [16, 8],
-      [17, 5],
-      [17, 6],
-      [17, 7],
-      [18, 6],
-      [21, 3],
-      [21, 4],
-      [21, 5],
-      [22, 3],
-      [22, 4],
-      [22, 5],
-      [23, 2],
-      [23, 6],
-      [25, 1],
-      [25, 2],
-      [25, 6],
-      [25, 7],
-      [35, 3],
-      [35, 4],
-      [36, 3],
-      [36, 4],
+      if (cell > 0) alive = count === 2 || count === 3;
+      else alive = count === 3;
 
-      // Random cells
-      // If you wait enough time these will eventually take part
-      // in destroying the glider gun, and the simulation will be in a "static" state.
-      [60, 47],
-      [61, 47],
-      [62, 47],
-      [60, 48],
-      [61, 48],
-      [62, 48],
-      [60, 49],
-      [61, 49],
-      [62, 49],
-      [60, 51],
-      [61, 51],
-      [62, 51]
-    ].forEach(point => (this.cells[point[0]][point[1]] = 1));
-
-    this.update();
-  }
-
-  update() {
-    let result = [];
-    const cells = this.cells;
-
-    function _countNeighbours(x, y) {
-      let amount = 0;
-
-      const _isAlive = (x, y) => cells[x] && cells[x][y];
-      const offsets = [-1, 0, 1];
-
-      offsets.forEach(offsetX =>
-        offsets.forEach(
-          offsetY => (amount += _isAlive(x + offsetX, y + offsetY))
-        )
-      );
-      amount -= _isAlive(x, y);
-
-      return amount;
-    }
-
-    this.cells.forEach((row, x) => {
-      result[x] = [];
-      row.forEach((cell, y) => {
-        let alive = 0,
-          count = _countNeighbours(x, y);
-
-        if (cell > 0) {
-          alive = count === 2 || count === 3;
-        } else {
-          alive = count === 3;
-        }
-
-        result[x][y] = alive;
-      });
+      result[x][y] = alive ? 1 : 0;
+      if (result[x][y]) cellsPositions.push([x, y]);
     });
+  });
 
-    this.cells = result;
+  cells = result;
+  result = [];
 
-    this.draw();
+  game.style.cssText = "--cells: " + cellsPositions.join(" ");
+  setTimeout(() => {
+    if (frame) frame = window.requestAnimationFrame(update);
+  }, 100);
+};
+
+const getCoordinatesMatrix = positions => {
+  const cells = [];
+  const cellsCount = 64;
+  for (let i = 0; i < cellsCount; i++) {
+    cells[i] = [];
+    for (let j = 0; j < cellsCount; j++) {
+      cells[i][j] = 0;
+    }
   }
 
-  draw() {
-    let canvas = this.canvas;
-    const cellSize = this.canvasSize / this.cellsCount;
+  positions.forEach(point => (cells[point[0]][point[1]] = 1));
+  return cells;
+};
 
-    canvas.clearRect(0, 0, this.canvasSize, this.canvasSize);
-    this.cells.forEach((row, x) => {
-      row.forEach((cell, y) => {
-        canvas.beginPath();
-        canvas.rect(x * cellSize, y * cellSize, cellSize, cellSize);
-        if (cell) {
-          canvas.fill();
-        } else {
-          canvas.stroke();
-        }
-      });
-    });
+let cellsPositions = coordinates;
+let cells = getCoordinatesMatrix(cellsPositions);
 
-    //I love js
-    setTimeout(this.update.bind(this), 10);
-    //window.requestAnimationFrame(update);
+game.style.cssText = "--cells: " + cellsPositions.join(" ");
+
+window.addEventListener("keydown", e => {
+  if (e.which == 96) {
+    frame = 1;
+    update();
   }
-}
 
-const game = new GameOfLife(64, 512, canvas);
+  if (e.which == 97) {
+    frame = null;
+    window.cancelAnimationFrame(frame);
+  }
+});
